b663e2152f585b47621a4411bf14e1ca7ac44a11
A1UM1 <88581492+a1um1@users.noreply.github.com>
2026-09-21 11:44:50 +0700

    add middleware

--- week5/app.mjs
+++ week5/app.mjs
@@ -1,33 +1,46 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const TODOS = [
   {
-    id: "task-01",
+    id: "1",
     title: "ทำการบ้าน",
     done: false,
     priority: "high",
   },
   {
-    id: "task-02",
+    id: "2",
     title: "ไปเที่ยว",
     done: false,
     priority: "normal",
   },
   {
-    id: "task-03",
+    id: "3",
     title: "ทำงาน",
     done: true,
     priority: "low",
   },
   {
-    id: "task-04",
+    id: "4",
     title: "อ่านหนังสือ",
     done: false,
     priority: "normal",
   },
 ];
+
+const PRIORITIES = ["low", "normal", "high"];
+
+function validateTodo(req, res, next) {
+  const { title, priority } = req.body;
+  if (typeof title !== "string" || title.trim() === "")
+    return res.status(400).json({ error: "title ต้องเป็น string และไม่ว่าง" });
+
+  if (!PRIORITIES.includes(priority)) return res.status(400).json({ error: `priority ไม่ถูกต้อง` });
+
+  next();
+}
 
 app
   .get("/", (req, res) => {
@@ -35,17 +48,34 @@ app
   })
   .get("/health", (req, res) => {
     res.json({ status: "ok" });
-  })
-  .get("/todos", (req, res) => {
+  });
+
+const todoRouter = express.Router(); //  Create new router for /api/todos
+
+todoRouter
+  .get("/", (req, res) => {
     res.json(TODOS);
   })
-  .get("/todos/:id", (req, res) => {
+  .get("/:id", (req, res) => {
     const { id } = req.params;
     const todo = TODOS.find((todo) => todo.id === id);
     if (!todo) return res.status(404).json({ error: `ไม่พบรายการ ${id}` });
 
     res.json(todo);
+  })
+  .post("/", validateTodo, (req, res) => {
+    const { title, priority } = req.body;
+    const newTodo = {
+      id: (TODOS.length + 1).toString(),
+      title,
+      done: false,
+      priority,
+    };
+    TODOS.push(newTodo);
+    res.status(201).json(newTodo);
   });
+
+app.use("/api/todos", todoRouter);
 
 const PORT = 3002;
 
